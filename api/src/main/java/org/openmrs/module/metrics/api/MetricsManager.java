2a61608f35e5f687896153d5d97994f4df8d72d4
Akayeshmantha <akayeshmantha@apache.org>
2020-05-15 20:32:28 +0200

    Write event to the db.

--- api/src/main/java/org/openmrs/module/metrics/api/MetricsManager.java
+++ api/src/main/java/org/openmrs/module/metrics/api/MetricsManager.java
@@ -1,16 +1,41 @@
 package org.openmrs.module.metrics.api;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
+import java.time.LocalDateTime;
 
+import org.apache.commons.lang3.StringUtils;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.openmrs.OpenmrsObject;
+import org.openmrs.api.context.Context;
 import org.openmrs.event.Event;
 import org.openmrs.module.DaemonToken;
+import org.openmrs.module.metrics.MetricEvent;
+import org.openmrs.module.metrics.api.db.EventAction;
+import org.openmrs.module.metrics.api.db.MetricsDAO;
 import org.openmrs.module.metrics.api.event.MetricsEventListener;
+import org.openmrs.module.metrics.api.exceptions.MetricsException;
+import org.openmrs.module.metrics.api.model.EventConfiguration;
+import org.openmrs.module.metrics.api.service.MetricService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.w3c.dom.events.EventException;
 
 public class MetricsManager {
-	
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(MetricsManager.class);
+
 	private DaemonToken token;
+
+	private static final String UUID_PATTERN = "{uuid}";
+
+	@Autowired
+	private MetricService metricService;
 	
 	public MetricsManager(DaemonToken token) {
 		this.token = token;
@@ -57,6 +82,48 @@ public class MetricsManager {
 			classesToMonitor = setOfClassesToMonitor;
 		} else {
 			classesToMonitor.addAll(setOfClassesToMonitor);
+		}
+	}
+
+	public void buildEventObject(OpenmrsObject openmrsObject, EventAction eventAction, EventConfiguration eventConfiguration){
+		if (!eventConfiguration.isEnabled()) {
+			LOGGER.debug("Skipped writing '{}' Event to the database because "
+							+ "the synchronization for this object is disabled in the configuration",
+					openmrsObject.getClass().getName());
+			return;
+		}
+
+		StringBuilder tags = new StringBuilder(eventAction.name());
+
+		final MetricEvent event = new MetricEvent(
+				UUID.randomUUID().toString(),
+				eventConfiguration.getTitle(),
+				LocalDateTime.now(),
+				null,
+				getEventContent(openmrsObject, eventConfiguration),
+				eventConfiguration.getCategory(),
+				tags.toString(),
+				LocalDateTime.now() //for now added current time stamp have to debug and see how the event object looks alike
+		);
+
+		metricService.saveMetricEvent(event);
+
+		LOGGER.info("An event from class {} has been saved in Events DB", openmrsObject.getClass().getName());
+	}
+
+	private String getEventContent(OpenmrsObject openmrsObject, EventConfiguration eventConfiguration) {
+
+		String uuid = openmrsObject.getUuid();
+		Map<String, String> urls = new HashMap<>();
+		for (Map.Entry<String, String> entry : eventConfiguration.getLinkTemplates().entrySet()) {
+			urls.put(entry.getKey(), entry.getValue().replace(UUID_PATTERN, uuid));
+		}
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		try {
+			return objectMapper.writeValueAsString(urls);
+		} catch (IOException e) {
+			throw new MetricsException("There is a problem with serialize resource links to Events content");
 		}
 	}
 }
