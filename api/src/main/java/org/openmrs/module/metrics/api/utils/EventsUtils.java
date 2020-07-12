5384386638a7eb79a7182fcfeffb9dc053e4a764
Akayeshmantha <akayeshmantha@apache.org>
2020-07-12 14:31:01 +0200

    add build event.

--- api/src/main/java/org/openmrs/module/metrics/api/utils/EventsUtils.java
+++ api/src/main/java/org/openmrs/module/metrics/api/utils/EventsUtils.java
@@ -2,19 +2,32 @@ package org.openmrs.module.metrics.api.utils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.Clock;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.io.IOUtils;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.openmrs.OpenmrsObject;
+import org.openmrs.event.Event;
+import org.openmrs.module.metrics.MetricEvent;
 import org.openmrs.module.metrics.api.exceptions.MetricsException;
+import org.openmrs.module.metrics.api.model.EventConfiguration;
 import org.openmrs.module.metrics.api.model.GeneralConfiguration;
 import org.openmrs.util.OpenmrsClassLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 public class EventsUtils {
 	
+	private static Clock clock = Clock.systemUTC();
+	
 	private static final Logger LOGGER = LoggerFactory.getLogger(EventsUtils.class);
+	
+	private static final String UUID_PATTERN = "{uuid}";
 	
 	public static boolean resourceFileExists(String path) {
 		InputStream in = OpenmrsClassLoader.getInstance().getResourceAsStream(path);
@@ -56,5 +69,42 @@ public class EventsUtils {
 			LOGGER.error(e.getMessage());
 			throw new MetricsException(e);
 		}
+	}
+	
+	public static MetricEvent buildEventObject(String simpleName, String uuid, Event.Action eventAction,
+	        EventConfiguration eventConfiguration) {
+		if (!eventConfiguration.isEnabled()) {
+			LOGGER.debug("Skipped writing '{}' Event to the database because "
+			        + "the synchronization for this object is disabled in the configuration", simpleName);
+			return null;
+		}
+		
+		StringBuilder tags = new StringBuilder(eventAction.name());
+		
+		final MetricEvent event = new MetricEvent(eventConfiguration.getTitle(), now(), null, getEventContent(simpleName,
+		    uuid, eventConfiguration), eventConfiguration.getCategory(), tags.toString()//for now added current time stamp have to debug and see how the event object looks alike
+		);
+		
+		return event;
+	}
+	
+	private static String getEventContent(String simpleName, String uuid, EventConfiguration eventConfiguration) {
+
+		String u_uid =  uuid;
+		Map<String, String> urls = new HashMap<>();
+		for (Map.Entry<String, String> entry : eventConfiguration.getLinkTemplates().entrySet()) {
+			urls.put(entry.getKey(), entry.getValue().replace(UUID_PATTERN, u_uid));
+		}
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		try {
+			return objectMapper.writeValueAsString(urls);
+		} catch (IOException e) {
+			throw new MetricsException("There is a problem with serialize resource links to Events content");
+		}
+	}
+	
+	public static Date now() {
+		return Date.from(Instant.now(clock));
 	}
 }
