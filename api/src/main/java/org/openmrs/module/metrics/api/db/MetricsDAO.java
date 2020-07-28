145bcdab389500f38a684734618b25f719296411
Ayeshmantha Perera <akayeshmantha@apache.org>
2020-07-28 14:13:38 +0200

    METRICS-6: Add method to retrieve events based on UUID (#15)

--- api/src/main/java/org/openmrs/module/metrics/api/db/MetricsDAO.java
+++ api/src/main/java/org/openmrs/module/metrics/api/db/MetricsDAO.java
@@ -5,7 +5,8 @@ import java.util.Map;
 import org.openmrs.module.metrics.MetricEvent;
 
 public interface MetricsDAO {
-	
+	MetricEvent getMetricEventByUuid(String uuid);
+
 	MetricEvent saveMetricEvent(MetricEvent metricEvent);
 	
 	Integer getEncounterObjectsByGivenDateRangeAndType(LocalDateTime startRange, LocalDateTime endRange, String encounterType);
