f5a651b5e969d919bf6cfdf0c06569a0b442c9df
Akayeshmantha <akayeshmantha@apache.org>
2020-08-05 12:56:16 +0200

    METRIC-9 : Add JMX report builder.

---
+++ api/src/main/java/org/openmrs/module/metrics/builder/JmxReportBuilderImpl.java
@@ -0,0 +1,38 @@
+package org.openmrs.module.metrics.builder;
+
+import java.util.concurrent.TimeUnit;
+
+import com.codahale.metrics.CachedGauge;
+import com.codahale.metrics.MetricRegistry;
+import org.openmrs.module.metrics.api.model.MetricscConfigImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class JmxReportBuilderImpl {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(JmxReportBuilderImpl.class);
+	
+	private static MetricRegistry metricRegistry;
+	
+	public MetricRegistry initializeMetricRegistry() {
+		return metricRegistry;
+	}
+	
+	@Autowired
+	public void setMetricRegistry(MetricRegistry metricRegistry) {
+		this.metricRegistry = metricRegistry;
+	}
+	
+	public MetricRegistry registerNewMetric(MetricRegistry metricRegistry, Object metricValue, String metricName) {
+		LOGGER.debug("Registering metric '{}' to the metric registry", metricName);
+		metricRegistry.register(metricRegistry.name(MetricscConfigImpl.class, metricName),
+		    new CachedGauge(15, TimeUnit.MINUTES) {
+			    @Override
+			    protected Object loadValue() {
+				    return metricValue;
+			    }
+		    });
+		return metricRegistry;
+	}
+}
